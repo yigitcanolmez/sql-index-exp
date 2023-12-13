0e28f857a5b124b6eef196a4cafb24f610546308
Yiğitcan Ölmez <90285509+yigitcanolmez@users.noreply.github.com>
2023-12-13 19:59:12 +0300

    added NonClustered

---
+++ non-clustered-index.sql
@@ -0,0 +1,16 @@
+select * from Person p 
+
+sp_Helpindex Person;
+
+DROP INDEX Person.IX_PERSON_EMAIL
+
+sp_Helpindex Person;
+
+select * from Person p ;
+
+CREATE NONCLUSTERED INDEX IX_PERSON_EMAIL ON Person (email ASC);
+
+sp_Helpindex Person;
+
+select * from Person p ;
+
